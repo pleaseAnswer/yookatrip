8c54df70b95d924f985aa49db3855aa2dccd0722
laozien <56870753+laozien@users.noreply.github.com>
2019-12-01 17:49:47 +0800

    登录注册

--- yoookatrip/src/pages/Mine.jsx
+++ yoookatrip/src/pages/Mine.jsx
@@ -1,10 +1,37 @@
 import React,{Component} from 'react';
-
+import '../css/mine.scss'
+import {Icon} from 'antd'
 class Mine extends Component{
+  goto=()=>{
+    this.props.history.push(`/login/`)
+  }
     render(){
         return (
-            <div>
-                我的
+            <div className="box-mine">
+               <header>
+                 <div className="boxmine">
+                   <Icon type="left" />
+                   <Icon type="unordered-list" />
+                   <Icon type="shopping-cart" />
+                   </div>
+               </header>
+
+                <div className="main">
+                 <div className="avatar-wrap">
+                   <div className="pic"></div>
+                   <div className="username">csdsdscscscsc</div>
+                   <div className="set"><Icon type="setting" />设置</div>
+                 </div>
+                 <ul className="nav-wrap">
+                     <li className="icon-angleright"><a href=""><i className="imgicon one"></i> <p>我的相册</p> <Icon type="right" /></a></li>
+                     <li className="icon-angleright"><a href=""><i className="imgicon two"></i><p>活动报名</p> <Icon type="right" /></a></li>
+                     <li className="icon-angleright"><a href=""><i className="imgicon three"></i><p>我的积分</p> <Icon type="right" /></a></li>
+                     <li className="icon-angleright"><a href=""><i className="imgicon four"></i><p> 站内消息</p><Icon type="right" /></a></li>
+                     <li className="icon-angleright"><a href=""><i className="imgicon fire"></i><p>网站通知</p><Icon type="right" /> </a></li>
+                 </ul>
+                 <div className="tuichu" onClick={this.goto.bind(this)}>退出</div>
+
+               </div>
             </div>
         )
     }
