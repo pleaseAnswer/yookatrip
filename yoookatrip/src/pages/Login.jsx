859a158d1cab83b83f9d7c3c9c84dca0d7c27ef8
qiunanyan <992679600@qq.com>
2019-12-05 12:02:06 +0800

    6

--- yoookatrip/src/pages/Login.jsx
+++ yoookatrip/src/pages/Login.jsx
@@ -1,82 +1,82 @@
-import React,{Component}  from 'react';
+import React, { Component } from 'react';
 import '../css/login.scss';
 import { my } from '../api'
 
 
-import  {Icon,Form, Input, Button, } from 'antd'
+import { Icon, Form, Input, Button, } from 'antd'
 
-class Login extends  Component{
+class Login extends Component {
 
-    goto = ()=>{
+    goto = () => {
         this.props.history.push(`/reg/`);
     }
-    
+
 
     handleSubmit = e => {
         e.preventDefault();
 
-        this.props.form.validateFields(async(err, values) => {
-          let { email, password } = values
-          console.log(email);
-          
-          if (!err) {
-            let { data } = await my.get("/database/login", {
-              email,
-              password
-          })
+        this.props.form.validateFields(async (err, values) => {
+            let { email, password } = values
 
-          console.log(data);
-          
-          if (data.status == 1) {
-              this.props.history.push(`/mine/`);
-          } else {
-           alert('注册失败')
-        }
-          }
-        });
-      };
-      submitForm=()=>{
 
-      }
+            if (!err) {
+                let { data } = await my.get("/database/login", {
+                    email,
+                    password
+                })
 
-      checkAccount(rule, value, callback) {
-        //与表单数据进行关联
-         
-          //正则用//包起来
-          var regex = /^((\+)?86|((\+)?86)?)0?1[3458]\d{9}$/; 
-          var reg =/^[a-z0-9A-Z]+[- | a-z0-9A-Z . _]+@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-z]{2,}$/
-          if (value) {
-            //react使用正则表达式变量的test方法进行校验，直接使用value.match(regex)显示match未定义
-            if (regex.test(value)||reg.test(value)) { 
-              callback();
-            } else { 
-              callback('请输入正确的手机号码!');
+
+
+                if (data.status == 1) {
+                    this.props.history.push(`/mine/`);
+                } else {
+                    alert('登录失败')
+                }
             }
-          } else {
-            //这里的callback函数会报错
-          }
-        }
-        iconL=()=>{
-          this.props.history.push(`/reg/`)
-      }
- 
-    render(){   
-        const { getFieldDecorator } = this.props.form;
-        
-        return (
-            
-            <div className="Box">
-               <header>
-                   <div className="box">
-                   <Icon type="left"  onClick={this.iconL.bind(this)}/>
-                   <span className="text">登录</span>
-                   <p className="p" onClick={this.goto.bind(this)}>注册</p>
-                   </div>
-               </header>
-                
-            <Form onSubmit={this.handleSubmit}  className="login-form">
+        });
+    };
+    submitForm = () => {
 
-            <Form.Item>
+    }
+
+    checkAccount(rule, value, callback) {
+        //与表单数据进行关联
+
+        //正则用//包起来
+        var regex = /^((\+)?86|((\+)?86)?)0?1[3458]\d{9}$/;
+        var reg = /^[a-z0-9A-Z]+[- | a-z0-9A-Z . _]+@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-z]{2,}$/
+        if (value) {
+            //react使用正则表达式变量的test方法进行校验，直接使用value.match(regex)显示match未定义
+            if (regex.test(value) || reg.test(value)) {
+                callback();
+            } else {
+                callback('请输入正确的手机号码!');
+            }
+        } else {
+            //这里的callback函数会报错
+        }
+    }
+    iconL = () => {
+        this.props.history.push(`/reg/`)
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+
+        return (
+
+            <div className="Box">
+                <header>
+                    <div className="box">
+                        <Icon type="left" onClick={this.iconL.bind(this)} />
+                        <span className="text">登录</span>
+                        <p className="p" onClick={this.goto.bind(this)}>注册</p>
+                    </div>
+                </header>
+
+                <Form onSubmit={this.handleSubmit} className="login-form">
+
+                    <Form.Item>
                         {getFieldDecorator('email', {
                             rules: [
                                 {
@@ -93,12 +93,12 @@ class Login extends  Component{
                             <Input
                                 prefix={<Icon type="user" />}
                                 placeholder="手机 / 邮箱"
-                         
+
                             />
                         )}
                     </Form.Item>
 
-            {/* <Form.Item>
+                    {/* <Form.Item>
                 {getFieldDecorator('email', {
                             rules: [
                                 {
@@ -119,25 +119,25 @@ class Login extends  Component{
                             />
                         )}
             </Form.Item> */}
-           
 
-            <Form.Item>
-            {getFieldDecorator('password', {
-                rules: [{ required: true, message: '请输入你的密码' }],
-            })(
-                <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="密码"
-                
-                />
-            )}
-            </Form.Item>
-                   
-                  
+
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button" onClick={this.submitForm.bind(this)}> 
-                        登录
+                        {getFieldDecorator('password', {
+                            rules: [{ required: true, message: '请输入你的密码' }],
+                        })(
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="密码"
+
+                            />
+                        )}
+                    </Form.Item>
+
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button" onClick={this.submitForm.bind(this)}>
+                            登录
                         </Button>
                     </Form.Item>
                     <div className="form-line">
@@ -149,27 +149,27 @@ class Login extends  Component{
                         <p className="three">第三方登录</p>
                         <ul>
                             <li><a href="https://graph.qq.com/oauth2.0/authorize?response_type=code&amp;client_id=101192654&amp;redirect_uri=http://www.saihuitong.com/thirdpart/qq/login&amp;state=W1604_0_0_%2Fuser">
-                                 <Icon type="qq"/>
-                             <p className="QQ">QQ</p> 
-                             </a></li>
+                                <Icon type="qq" />
+                                <p className="QQ">QQ</p>
+                            </a></li>
                             <li><a href="https://api.weibo.com/oauth2/authorize?client_id=1495759747&amp;redirect_uri=http://www.saihuitong.com/thirdpart/weibo/login&amp;state=W1604_0_0_%2Fuser&amp;scope=all&amp;display=default&amp;forcelogin=false">
                                 <Icon type="weibo-square" />
-                             <p className="wb">微博</p>
-                              </a></li>
-                            
+                                <p className="wb">微博</p>
+                            </a></li>
+
                         </ul>
                     </div>
-                 </Form>
+                </Form>
 
 
-              
+
             </div>
-     
+
         )
     }
 }
 
 const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(Login);
 
-  
-export default  WrappedNormalLoginForm;
+
+export default WrappedNormalLoginForm;
